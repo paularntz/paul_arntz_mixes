5d5a6d45f93eab04e0d7f5d7087bf76504e07a37
Paul Arntz <paul@paularntz.com>
2024-04-13 03:29:28 -0500

    contact form update 3

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,7 +2,7 @@
 const Page = () => {
     return (
         <>
-            <form name="contact" data-netlify="true"  className="container grid grid-cols-1 gap-4">
+            <form name="contact" data-netlify="true"  method="POST" className="container grid grid-cols-1 gap-4">
                 <p>
                     <label>Name <input type="text" name="name" /></label>
                 </p>
