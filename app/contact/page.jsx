08a058154ff6ea1a594660bd2049a17c0462cf44
Paul Arntz <parntz@encirca.com>
2024-08-09 12:16:49 -0500

    more contact form stuff

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -36,6 +36,7 @@ export default function ContactPage() {
         netlify
       >
         <input type="hidden" name="form-name" value="contact" />
+        <input type="hidden" name="redirect" value="/thank-you" />
         <h2 className="text-2xl font-semibold text-center mb-6">Contact Us</h2>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
---
+++ app/thank-you/page.jsx
@@ -0,0 +1,12 @@
+"use client";  // This directive tells Next.js to treat this as a Client Component
+
+import React, { useState } from "react";
+
+export default function ThankYou() {
+    return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1>Thank You!</h1>
+      <h3>I'll get back with you ASAP!</h3>
+    </div>
+  );
+}
