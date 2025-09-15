2c27c1729d17712488dad8d240dd27cba88f494a
Paul Arntz <paul@paularntz.com>
2025-09-15 14:55:13 -0500

    tequila mockingbird

--- app/mixes/page.jsx
+++ app/mixes/page.jsx
@@ -20,7 +20,7 @@ const tracks = [
     {
         key: "1",
         url: "https://www.dropbox.com/scl/fi/4yorck0oej4d28bvw9606/2025-09-14-Pile-Driving-Miss-Daisy.wav?rlkey=24q1wquox140baf4gn770dzsz&raw=1",
-        title: "Tasteless and Offensive",
+        title: "Tequila Mockingbird",
         artist: "Jeff Berlin",
         type: "audio/wav",
     },
